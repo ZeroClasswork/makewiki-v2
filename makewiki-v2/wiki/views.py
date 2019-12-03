3163139cbad54d152be4fe4be5a6c473a8e7b26a
atleastzero <megan.obryan52@gmail.com>
2019-12-03 14:19:27 -0800

    Adding templates/views

--- makewiki-v2/wiki/views.py
+++ makewiki-v2/wiki/views.py
@@ -1,8 +1,12 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 from wiki.models import Page
+from wiki.forms import PageCreateForm
 
 
 class PageListView(ListView):
@@ -26,3 +30,23 @@ class PageDetailView(DetailView):
         return render(request, 'page.html', {
           'page': page
         })
+
+class PageCreateView(CreateView):
+    model = Page
+
+    def get(self, request, *args, **kwargs):
+        context = {'form': PageCreateForm()}
+        return render(request, 'pages/create.html', context)
+
+    def post(self, request, *args, **kwargs):
+      form = PageCreateForm(request.POST)
+      pages = self.get_queryset().all()
+      if form.is_valid():
+          page = form.save()
+          page.save()
+          return render(request, 'page.html', {
+            'page': page
+          })
+      return render(request, 'list.html', {
+        'pages': pages
+      })
