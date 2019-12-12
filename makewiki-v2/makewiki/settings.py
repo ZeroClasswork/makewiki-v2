6066fb34f0950b5fbf547c7b47a1f768934226e7
atleastzero <megan.obryan52@gmail.com>
2019-12-12 14:37:37 -0800

    Updated for deployment

--- makewiki-v2/makewiki/settings.py
+++ makewiki-v2/makewiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'meg-wiki.herokuapp.com']
 
 
 # Application definition
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
